6ed243a10045fad3bf29e3fa96892991f9d19e54
baisalikhan <112929397+baisalikhan@users.noreply.github.com>
2026-09-10 01:10:59 +0500

    feat: implement project scaffolding with custom theme, icon constants, and tab navigation layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
+import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-green-500">
         Welcome to Nativewind!
       </Text>
@@ -25,6 +30,6 @@ export default function Index() {
       <Link href={{ pathname: '/subscriptions/[id]', params: { id: 'slack-pro' } }} asChild >
         <Text className="mt-4 rounded bg-primary p-4 text-white">Slack Pro Subscription</Text>
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/subscriptions/[id].tsx
+++ app/subscriptions/[id].tsx
@@ -1,14 +1,18 @@
 import { Link, useLocalSearchParams } from 'expo-router';
+import { styled } from "nativewind";
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const SubscriptionsDetails = () => {
     const { id } = useLocalSearchParams<{ id: string }>();
     return (
-        <View>
+        <SafeAreaView>
             <Text>Subscriptions Details {id}</Text>
             <Link href="../">Go to parent</Link>
-        </View>
+        </SafeAreaView>
     );
 };
 
