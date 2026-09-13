4636c7832e21e32c936a2d252ed66a65fc601a59
baisalikhan <112929397+baisalikhan@users.noreply.github.com>
2026-09-14 02:29:46 +0500

    feat: home page ui completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,36 @@
 import '@/global.css';
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { useEffect } from 'react';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const [fontsLoaded, fontError] = useFonts({
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      console.log('Fonts loaded');
+    }
+    if (fontError) {
+      console.log('Font error:', fontError);
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }} initialRouteName="(tabs)">
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
